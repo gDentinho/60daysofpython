ebfbb1dd86eef6a991bd5fccfa9f3c14e6437a79
gDentinho <gustavomdentinho@gmail.com>
2025-01-01 18:54:56 -0300

    Adicionado o dia 02 programando em python, e atualizado o README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 60 dias de programação em Python
 - [x] Dia 1 - Olá Mundo em Python
+- [x] Dia 2 - soma de dois numero de duas formas diferentes
+- [ ] Dia 3 - 
---
+++ day02/somar_dois_numeros.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro Numero: "))
+
+soma_dois_numero = numero1 + numero2
+
+print(f"A soma de {numero1} + {numero2} = {soma_dois_numero}")
